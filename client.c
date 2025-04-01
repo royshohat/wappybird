f38fb239dc2654b3e3dea72d9a5117fc04e11a37
royshohat <roy123123sohat@gmail.com>
2025-04-01 18:27:30 +0300

    test2
    Co-authored-by: MaftehShvedi <Melafefonim@users.noreply.github.com>

---
+++ client.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+SDL_Window* gWindow = NULL;
+
+bool make_window() {
+    
+}
+
+typedef struct{
+
+};
+#define HEIGHT 540
+#define WIDTH 960
+
+int main(void){
+
+    return 0;
+}
