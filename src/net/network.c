31a891d7cf9a0defb06a9b133eb3ee3b5ac26212
Paz David <pazdavid2008@gmail.com>
2025-07-08 21:25:40 +0300

    Fix source files

--- src/net/network.c
+++ src/net/network.c
@@ -1,117 +1,115 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <errno.h>
 #include <string.h>
-#include <fcntl.h>
-#include <limits.h>
 #include <sys/socket.h>
 
 #include "common/game.h"
-#include "networking/net_const.h"
-#include "networking/net.h"
-#include "networking/network.h"
+#include "net/net.h"
+#include "net/net_const.h"
+#include "net/network.h"
 
 uint32_t get_packet_size(packet_type type) {
-    switch (type) {
-        case TYPE_REQ_LEAVE:
-            return SIZE_REQ_LEAVE;
-        case TYPE_BROADCAST_LEAVE:
-            return SIZE_BROADCAST_JOIN;
-        case TYPE_REQ_PING:
-            return SIZE_REQ_PING;
-        case TYPE_RESP_PING:
-            return SIZE_RESP_PING;
-        case TYPE_RESP_JOIN:
-            return SIZE_RESP_JOIN;
-        case TYPE_REQ_JOIN:
-            return SIZE_REQ_JOIN;
-        case TYPE_BROADCAST_JOIN:
-            return SIZE_BROADCAST_JOIN;
-        case TYPE_REQ_READY:
-            return SIZE_REQ_READY;
-        case TYPE_BROADCAST_READY:
-            return SIZE_BROADCAST_READY;
-        case TYPE_REQ_TIMESTAMP:
-            return SIZE_REQ_TIMESTAMP;
-        case TYPE_RESP_TIMESTAMP:
-            return SIZE_RESP_TIMESTAMP;
-        case TYPE_BROADCAST_START_GAME:
-            return SIZE_BROADCAST_START_GAME;
-        case TYPE_REQ_UPDATE_STATE:
-            return SIZE_REQ_UPDATE_STATE;
-        case TYPE_BROADCAST_UPDATE_STATE:
-            return SIZE_BROADCAST_UPDATE_STATE;
-        default: 
-            printf("in function get_packet_size: No such type. %d\n", type);
-            return 0;
-    }
+  switch (type) {
+  case TYPE_REQ_LEAVE:
+    return SIZE_REQ_LEAVE;
+  case TYPE_BROADCAST_LEAVE:
+    return SIZE_BROADCAST_JOIN;
+  case TYPE_REQ_PING:
+    return SIZE_REQ_PING;
+  case TYPE_RESP_PING:
+    return SIZE_RESP_PING;
+  case TYPE_RESP_JOIN:
+    return SIZE_RESP_JOIN;
+  case TYPE_REQ_JOIN:
+    return SIZE_REQ_JOIN;
+  case TYPE_BROADCAST_JOIN:
+    return SIZE_BROADCAST_JOIN;
+  case TYPE_REQ_READY:
+    return SIZE_REQ_READY;
+  case TYPE_BROADCAST_READY:
+    return SIZE_BROADCAST_READY;
+  case TYPE_REQ_TIMESTAMP:
+    return SIZE_REQ_TIMESTAMP;
+  case TYPE_RESP_TIMESTAMP:
+    return SIZE_RESP_TIMESTAMP;
+  case TYPE_BROADCAST_START_GAME:
+    return SIZE_BROADCAST_START_GAME;
+  case TYPE_REQ_UPDATE_STATE:
+    return SIZE_REQ_UPDATE_STATE;
+  case TYPE_BROADCAST_UPDATE_STATE:
+    return SIZE_BROADCAST_UPDATE_STATE;
+  default:
+    printf("in function get_packet_size: No such type. %d\n", type);
+    return 0;
+  }
 }
 
 void clear_socket_buffer(int sockfd) {
-    // set socket flag to non blocking
-    long n; 
-    int data[1024];
-    int flags = fcntl(sockfd, F_GETFL, 0) | O_NONBLOCK;
-    fcntl(sockfd, F_SETFL, flags);
-    
-    do {
-        n = recv(sockfd, data, sizeof(data), 0);
-    } while (n > 0);
-    // the buffer is now empty :)
-    // unset block flag
-    fcntl(sockfd, F_SETFL, flags & (O_NONBLOCK ^ INT_MAX));
+  // set socket flag to non blocking
+  long n;
+  int data[1024];
+  int flags = fcntl(sockfd, F_GETFL, 0) | O_NONBLOCK;
+  fcntl(sockfd, F_SETFL, flags);
+
+  do {
+    n = recv(sockfd, data, sizeof(data), 0);
+  } while (n > 0);
+  // the buffer is now empty :)
+  // unset block flag
+  fcntl(sockfd, F_SETFL, flags & (O_NONBLOCK ^ INT_MAX));
 }
 
+void print_packet(packet_type type, packet_fields *fields) {
+  printf("TYPE: %d\n", type);
+  char data[MAX_DATA_LENGTH];
 
-void print_packet(packet_type type, packet_fields* fields){
-    printf("TYPE: %d\n", type);
-    char data[MAX_DATA_LENGTH];
+  switch (type) {
+  case TYPE_BROADCAST_LEAVE:
+    *(uint32_t *)(data) = fields->id;
+    break;
+  case TYPE_REQ_PING:
+    // no data.
+    break;
+  case TYPE_RESP_JOIN:
+    *(uint32_t *)(data) = fields->id;
+    break;
+  case TYPE_BROADCAST_JOIN:
+    *(uint32_t *)(data) = fields->id;
+    break;
+  case TYPE_BROADCAST_READY:
+    *(data) = fields->is_ready;
+    *(uint32_t *)(data + 1) = fields->id;
+    break;
+  case TYPE_REQ_TIMESTAMP:
+    *(uint64_t *)(data) = fields->timestamp;
+    break;
+  case TYPE_BROADCAST_START_GAME: // TODO
+    break;
+  case TYPE_BROADCAST_UPDATE_STATE:
+    memcpy(data, fields->players_array, SIZE_BROADCAST_UPDATE_STATE);
+    break;
+  case TYPE_RESP_PING:
+    break;
+  case TYPE_REQ_READY:
+    *(data) = fields->is_ready;
+    break;
+  case TYPE_RESP_TIMESTAMP:
+    *(uint64_t *)(data) = fields->timestamp;
+    break;
+  case TYPE_REQ_UPDATE_STATE: // TODO
+    break;
+  default:
+    printf("send: not implemented.\n");
+    return;
+  }
 
-
-    switch (type) {
-        case TYPE_BROADCAST_LEAVE:
-            *(uint32_t*) (data) = fields->id;
-            break;
-        case TYPE_REQ_PING:
-            // no data.
-            break;
-        case TYPE_RESP_JOIN:
-            *(uint32_t*) (data) = fields->id;
-            break;
-        case TYPE_BROADCAST_JOIN:
-            *(uint32_t*) (data ) = fields->id;
-            break;
-        case TYPE_BROADCAST_READY:
-            *(data) = fields->is_ready;
-            *(uint32_t*) (data + 1) = fields->id;
-            break;
-        case TYPE_REQ_TIMESTAMP:
-            *(uint64_t*) (data ) = fields->timestamp;
-            break;
-        case TYPE_BROADCAST_START_GAME: // TODO
-            break;
-        case TYPE_BROADCAST_UPDATE_STATE:
-            memcpy(data, fields->players_array, SIZE_BROADCAST_UPDATE_STATE);
-            break;
-        case TYPE_RESP_PING:
-            break;
-        case TYPE_REQ_READY:
-            *(data) = fields->is_ready;
-            break;
-        case TYPE_RESP_TIMESTAMP:
-            *(uint64_t*) (data) = fields->timestamp;
-            break;
-        case TYPE_REQ_UPDATE_STATE: // TODO
-            break;
-        default: 
-            printf("send: not implemented.\n");
-            return;
-    }
-
-    printf("DATA: ");
-    for (int i = 0; i < get_packet_size(type); i++){
-        printf("%x ", data[i]);
-    }
-    printf("\n");
+  printf("DATA: ");
+  for (int i = 0; i < get_packet_size(type); i++) {
+    printf("%x ", data[i]);
+  }
+  printf("\n");
 }
--- src/server/server_net.c
+++ src/server/server_net.c
@@ -1,198 +1,198 @@
+#include "common/game.h"
+#include "net/net_util.h"
+
+#include <arpa/inet.h> // for sockaddr_in, inet_pton()
 #include <stdarg.h>
 #include <stdint.h>
-#include <sys/socket.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <arpa/inet.h> // for sockaddr_in, inet_pton()
+#include <sys/socket.h>
+#include <unistd.h>
 
-#include "common/game.h"
-#include "networking/network.h"
-#include "networking/net_const.h"
-#include "networking/net.h"
+int send_packet(int fd, packet_type type, packet_fields *fields) {
+  if (fd <= 0) {
+    perror("Invalid file desc.");
+    return 1;
+  }
+  char msg_buffer[SIZE_HEADER + MAX_DATA_LENGTH];
 
-int send_packet(int fd, packet_type type, packet_fields* fields) {
-    if (fd <= 0) {
-        perror("Invalid file desc.");
-        return 1;
+  // construct the header
+  // uint32_t size = get_packet_size(type);
+  // *(uint32_t*) (msg_buffer + 1) = size;
+
+  *msg_buffer = type;
+  // 1. transform project to use select
+  // 2. implement ping in server: (in handle client recv packet as usual and add
+  // a case for ping in the switch statment)
+  //
+  switch (type) {
+  case TYPE_BROADCAST_LEAVE:
+    *(uint32_t *)(msg_buffer + SIZE_HEADER) = fields->id;
+    break;
+  case TYPE_RESP_PING:
+    // no data.
+    break;
+  case TYPE_RESP_JOIN:
+    *(uint32_t *)(msg_buffer + SIZE_HEADER) = fields->id;
+    break;
+  case TYPE_BROADCAST_JOIN:
+    *(uint32_t *)(msg_buffer + SIZE_HEADER) = fields->id;
+    break;
+  case TYPE_BROADCAST_READY:
+    *(msg_buffer + SIZE_HEADER) = fields->is_ready;
+    *(uint32_t *)(msg_buffer + SIZE_HEADER + 1) = fields->id;
+    break;
+  case TYPE_REQ_TIMESTAMP:
+    *(uint64_t *)(msg_buffer + SIZE_HEADER) = fields->timestamp;
+    break;
+  case TYPE_BROADCAST_START_GAME: // TODO
+    break;
+  case TYPE_BROADCAST_UPDATE_STATE:
+    memcpy(msg_buffer + SIZE_HEADER, fields->players_array,
+           SIZE_BROADCAST_UPDATE_STATE);
+    break;
+  default:
+    printf("send: not implemented.\n");
+    return 1;
+  }
+
+  for (uint32_t i = 0; i < SIZE_HEADER + get_packet_size(type); i++) {
+    printf("%x ", msg_buffer[i]);
+  }
+  printf("\n");
+  send(fd, msg_buffer, SIZE_HEADER + get_packet_size(type), 0);
+  return 0;
+}
+
+packet_type recv_packet(int fd, packet_fields *fields) {
+  if (fd <= 0) {
+    perror("Invalid file desc.");
+    return TYPE_INVALID;
+  }
+  packet_type type = TYPE_REQ_LEAVE;
+
+  char msg_buffer[MAX_DATA_LENGTH];
+  recv(fd, &type, 1, 0);
+
+  switch (type) {
+  case TYPE_REQ_LEAVE:
+    // if fd is closed
+    return TYPE_REQ_LEAVE;
+  case TYPE_REQ_PING:
+    // no data.
+    break;
+  case TYPE_REQ_JOIN:
+    break;
+  case TYPE_REQ_READY:
+    recv(fd, &(fields->is_ready), sizeof(fields->is_ready), 0);
+    break;
+  case TYPE_RESP_TIMESTAMP:
+    recv(fd, &(fields->timestamp), sizeof(fields->timestamp), 0);
+    break;
+  case TYPE_REQ_UPDATE_STATE:
+    // no data?
+    break;
+  default:
+    printf("recv: not implemented\n");
+    return TYPE_INVALID;
+  }
+  return type;
+}
+
+int handle_packet(int fd, packet_type type, packet_fields *fields,
+                  player_t *players_arr, size_t *player_count) {
+  // not thread safe
+  // destroys fields.
+  switch (type) {
+  case TYPE_REQ_PING:
+
+    break;
+  case TYPE_REQ_LEAVE:
+    for (int i = 0; i < MAX_PLAYER_COUNT; i++) {
+      // set the player with the current fd to is_active = false.
+      if (!players_arr[i].client.is_active)
+        continue;
+      if (players_arr[i].client.fd == fd) {
+        players_arr[i].client.is_active = false;
+        close(fd);
+        break;
+      }
     }
-    char msg_buffer[SIZE_HEADER + MAX_DATA_LENGTH];
-    
-    // construct the header
-    // uint32_t size = get_packet_size(type);
-    // *(uint32_t*) (msg_buffer + 1) = size;
+    // decrement player count
+    (*player_count)--;
 
-    *msg_buffer = type;
-    // 1. transform project to use select
-    // 2. implement ping in server: (in handle client recv packet as usual and add a case for ping in the switch statment)
-    // 
-    switch (type) {
-        case TYPE_BROADCAST_LEAVE:
-            *(uint32_t*) (msg_buffer + SIZE_HEADER) = fields->id;
-            break;
-        case TYPE_RESP_PING:
-            // no data.
-            break;
-        case TYPE_RESP_JOIN:
-            *(uint32_t*) (msg_buffer + SIZE_HEADER) = fields->id;
-            break;
-        case TYPE_BROADCAST_JOIN:
-            *(uint32_t*) (msg_buffer + SIZE_HEADER) = fields->id;
-            break;
-        case TYPE_BROADCAST_READY:
-            *(msg_buffer + SIZE_HEADER) = fields->is_ready;
-            *(uint32_t*) (msg_buffer + SIZE_HEADER + 1) = fields->id;
-            break;
-        case TYPE_REQ_TIMESTAMP:
-            *(uint64_t*) (msg_buffer + SIZE_HEADER) = fields->timestamp;
-            break;
-        case TYPE_BROADCAST_START_GAME: // TODO
-            break;
-        case TYPE_BROADCAST_UPDATE_STATE:
-            memcpy(msg_buffer+SIZE_HEADER, fields->players_array, SIZE_BROADCAST_UPDATE_STATE);
-            break;
-        default: 
-            printf("send: not implemented.\n");
-            return 1;
+    break;
+  case TYPE_REQ_JOIN:
+    // find the current player object (stored in player struct)
+    player_t *player;
+    for (int i = 0; i < MAX_PLAYER_COUNT; i++) {
+      if (!players_arr[i].client.is_active)
+        continue;
+      if (players_arr[i].client.fd == fd) {
+        if (players_arr[i].is_alive)
+          return 1; // already joined
+        player = (players_arr + i);
+        break;
+      }
     }
+    player->id = fd;
+    player->is_ready = false;
+    player->is_alive = true;
 
-    for (int i = 0; i < SIZE_HEADER + get_packet_size(type); i++){
-        printf("%x ", msg_buffer[i]);
-    }
-    printf("\n");
-    send(fd, msg_buffer, SIZE_HEADER + get_packet_size(type), 0);
-    return 0;
-} 
-       
-
-
-packet_type recv_packet(int fd, packet_fields* fields){
-    if (fd <= 0) {
-        perror("Invalid file desc.");
-        return TYPE_INVALID;
-    }
-    packet_type type = TYPE_REQ_LEAVE;
-    
-
-    char msg_buffer[MAX_DATA_LENGTH];
-    recv(fd, &type, 1, 0);
-
-    switch (type) {
-        case TYPE_REQ_LEAVE:
-            // if fd is closed
-            return TYPE_REQ_LEAVE;
-        case TYPE_REQ_PING:
-            // no data.
-            break;
-        case TYPE_REQ_JOIN:
-            break;
-        case TYPE_REQ_READY:
-            recv(fd, &(fields->is_ready), sizeof(fields->is_ready), 0);
-            break;
-        case TYPE_RESP_TIMESTAMP:
-            recv(fd, &(fields->timestamp), sizeof(fields->timestamp), 0);
-            break;
-        case TYPE_REQ_UPDATE_STATE:
-            // no data?
-            break;
-        default: 
-            printf("recv: not implemented\n");
-            return TYPE_INVALID;
-    }
-    return type;
-} 
-
-int handle_packet(int fd, packet_type type, packet_fields* fields, player_t* players_arr, size_t* player_count) {
-    // not thread safe
-    // destroys fields.
-    switch (type) {
-        case TYPE_REQ_PING:
-            
-            break;
-        case TYPE_REQ_LEAVE:
-            for (int i = 0; i < MAX_PLAYER_COUNT; i++) {
-                // set the player with the current fd to is_active = false.
-                if (!players_arr[i].client.is_active) continue;
-                if (players_arr[i].client.fd == fd) {
-                    players_arr[i].client.is_active = false;
-                    close(fd);
-                    break;
-                }
-            }
-            // decrement player count
-            (*player_count)--;
-            
-            break;
-        case TYPE_REQ_JOIN:
-            // find the current player object (stored in player struct)
-            player_t* player;
-            for (int i = 0; i < MAX_PLAYER_COUNT; i++) {
-                if (!players_arr[i].client.is_active) continue;
-                if (players_arr[i].client.fd == fd) {
-                    if (players_arr[i].is_alive) return 1; // already joined
-                    player = (players_arr + i);
-                    break;
-                }
-            }
-            player->id = fd;
-            player->is_ready = false;
-            player->is_alive = true;
-
-                    
-            for (int i = 0; i < MAX_PLAYER_COUNT; i++) {
-                if (!players_arr[i].client.is_active ||
-                     (players_arr + i) == player) continue;
-                fields->id = player->id;
-                send_packet(player->client.fd, TYPE_BROADCAST_JOIN, fields);
-            }
-
-            break;
-        case TYPE_REQ_READY:
-            break;
-        case TYPE_RESP_TIMESTAMP:
-            break;
-        case TYPE_REQ_UPDATE_STATE:
-            break;
-        default: 
-            printf("recv: not implemented\n");
-            break;
-    }
-    return 0; 
-} 
-
-
-
-int init(vars_t* vars) {
-    vars->server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (vars->server_fd == -1) {
-        perror("Error creating a socket.");
-        return 1;
+    for (int i = 0; i < MAX_PLAYER_COUNT; i++) {
+      if (!players_arr[i].client.is_active || (players_arr + i) == player)
+        continue;
+      fields->id = player->id;
+      send_packet(player->client.fd, TYPE_BROADCAST_JOIN, fields);
     }
 
-    int enabled = 1;
-    (void)setsockopt(vars->server_fd, SOL_SOCKET, SO_REUSEADDR, &enabled, sizeof(enabled)); 
-    // nice to have, lets run bin files in a row 
-    // delete in production.
-    
-    memset(&vars->server_addr, 0, sizeof(vars->server_addr));
-    vars->server_addr.sin_family = AF_INET; 
-    vars->server_addr.sin_addr.s_addr = INADDR_ANY;
-    vars->server_addr.sin_port = htons(TCP_PORT); 
-    
-    if(bind(vars->server_fd, (struct sockaddr *)&vars->server_addr, sizeof(vars->server_addr))==-1){
-        perror ("Error binding address.");
-        close(vars->server_fd);
-        return 1;
-    }
-    // listen for new connections
-    if(listen(vars->server_fd, MAX_PLAYER_COUNT)==-1){
-        perror ("Error Listening to socket.");
-        close(vars->server_fd);
-        return 1;
-    }
+    break;
+  case TYPE_REQ_READY:
+    break;
+  case TYPE_RESP_TIMESTAMP:
+    break;
+  case TYPE_REQ_UPDATE_STATE:
+    break;
+  default:
+    printf("recv: not implemented\n");
+    break;
+  }
+  return 0;
+}
 
-    // clients array init
-    memset(vars->clients, 0, MAX_CLIENT_COUNT * sizeof(client_t));
+int init(vars_t *vars) {
+  vars->server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (vars->server_fd == -1) {
+    perror("Error creating a socket.");
+    return 1;
+  }
 
-    return 0;
+  int enabled = 1;
+  (void)setsockopt(vars->server_fd, SOL_SOCKET, SO_REUSEADDR, &enabled,
+                   sizeof(enabled));
+  // nice to have, lets run bin files in a row
+  // delete in production.
+
+  memset(&vars->server_addr, 0, sizeof(vars->server_addr));
+  vars->server_addr.sin_family = AF_INET;
+  vars->server_addr.sin_addr.s_addr = INADDR_ANY;
+  vars->server_addr.sin_port = htons(TCP_PORT);
+
+  if (bind(vars->server_fd, (struct sockaddr *)&vars->server_addr,
+           sizeof(vars->server_addr)) == -1) {
+    perror("Error binding address.");
+    close(vars->server_fd);
+    return 1;
+  }
+  // listen for new connections
+  if (listen(vars->server_fd, MAX_PLAYER_COUNT) == -1) {
+    perror("Error Listening to socket.");
+    close(vars->server_fd);
+    return 1;
+  }
+
+  // clients array init
+  memset(vars->clients, 0, MAX_CLIENT_COUNT * sizeof(client_t));
+
+  return 0;
 }
